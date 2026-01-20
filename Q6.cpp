835a282efeda5ebbc9647646dead666487a00e1c
poorva25csu178 <poorva25csu178@ncuindia.edu>
2026-01-20 11:03:29 +0530

    bonus

---
+++ Q6.cpp
@@ -0,0 +1,16 @@
+float basic_salary, bonus=1.12, no , n;
+
+    cout<<"Enter total no. of employees: ";
+    cin>>n;
+
+    for (int i=1; i<=n; i++){
+
+    cout<<"\n Enter no. of employee: ";
+    cin>>no;
+
+    cout<<"\n Enter basic salary of employee: ";
+    cin>>basic_salary;
+
+    cout<<"\n Net salary of employee: "<<basic_salary*bonus;
+
+    }
